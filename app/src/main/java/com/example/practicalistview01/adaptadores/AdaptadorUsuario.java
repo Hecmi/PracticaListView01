4c9bea5112977c7f8ebe2ea66fdde6c4c1b16ef8
Hecmi <hcasanovam@uteq.edu.ec>
2023-07-06 10:55:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/practicalistview01/adaptadores/AdaptadorUsuario.java
@@ -0,0 +1,44 @@
+package com.example.practicalistview01.adaptadores;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.example.practicalistview01.R;
+
+import java.util.ArrayList;
+
+import com.example.practicalistview01.modelos.Usuario;
+
+public class AdaptadorUsuario extends ArrayAdapter<Usuario> {
+
+    public AdaptadorUsuario(Context context, ArrayList<Usuario> datos) {
+        super(context, R.layout.item_usuario_layout, datos);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater =LayoutInflater.from(getContext());
+        View item = inflater.inflate(R.layout.item_usuario_layout, null);
+
+        TextView lblNombre  = (TextView)item.findViewById(R.id.lblNombre);
+        TextView lblEmail  = (TextView)item.findViewById(R.id.lblEmail);
+        TextView lblWeb  = (TextView)item.findViewById(R.id.lblweb);
+
+        lblNombre.setText(getItem(position).getNombre() + " " + getItem(position).getApellido());
+        lblEmail.setText(getItem(position).getEmail());
+        lblWeb.setText(getItem(position).getUrl_avatar());
+
+        ImageView imgView =(ImageView)item.findViewById(R.id.imgUsr);
+
+        Glide.with(this.getContext()).load(getItem(position).getUrl_avatar()).into(imgView);
+
+        return item;
+    }
+}
